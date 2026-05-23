4afdee7cc6543193740f6511de8a3242f23c48d2
werapi <sokneip@tuta.io>
2026-05-23 20:34:22 +0200

    Add support for Pine64 PineNote

---
+++ OpenTabletDriver.Configurations/Parsers/Pine64/PineNoteButtonReport.cs
@@ -0,0 +1,32 @@
+using System.Numerics;
+using OpenTabletDriver.Plugin.Tablet;
+
+namespace OpenTabletDriver.Configurations.Parsers.Pine64
+{
+    public struct PineNoteButtonReport : ITabletReport
+    {
+        public PineNoteButtonReport(byte[] report, ref Vector2 lastPosition, ref uint lastPressure, ref bool[] lastPenButtons)
+        {
+            Raw = report;
+
+            Position = lastPosition;
+            Pressure = lastPressure;
+
+            PenButtons = new bool[]
+            {
+                report[1].IsBitSet(0),
+                report[1].IsBitSet(1),
+                report[1].IsBitSet(2),
+                report[1].IsBitSet(3),
+                report[1].IsBitSet(4),
+            };
+
+            lastPenButtons = PenButtons;
+        }
+
+        public byte[] Raw { set; get; }
+        public Vector2 Position { set; get; }
+        public uint Pressure { set; get; }
+        public bool[] PenButtons { set; get; }
+    }
+}
---
+++ OpenTabletDriver.Configurations/Parsers/Pine64/PineNoteReportParser.cs
@@ -0,0 +1,25 @@
+using System.Numerics;
+using OpenTabletDriver.Plugin.Tablet;
+
+namespace OpenTabletDriver.Configurations.Parsers.Pine64
+{
+    public class PineNoteReportParser : IReportParser<IDeviceReport>
+    {
+        private Vector2 _lastPosition;
+        private uint _lastPressure;
+        private bool[] _lastPenButtons = new bool[5];
+
+        public IDeviceReport Parse(byte[] report)
+        {
+            switch (report[0])
+            {
+                case 2:
+                    return new PineNoteTabletReport(report, ref _lastPosition, ref _lastPressure, ref _lastPenButtons);
+                case 1:
+                    return new PineNoteButtonReport(report, ref _lastPosition, ref _lastPressure, ref _lastPenButtons);
+                default:
+                    return null;
+            }
+        }
+    }
+}
